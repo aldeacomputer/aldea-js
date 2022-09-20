0e9af1eb72c8a2af83a8e69be32f1dc899961a63
Brenton Gunning <brentongunning@gmail.com>
2022-09-20 09:25:40 -0600

    Initial spec for endpoints

--- node/index.js
+++ node/index.js
@@ -2,8 +2,34 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/status', (req, res) => {
+  res.send('OK')
+})
+
+app.get('/tx/:txid', (req, res) => {
+  // TODO
+  res.send('OK')
+})
+
+app.get('/state/:location', (req, res) => {
+  // TODO
+  res.send('OK')
+})
+
+app.post('/tx', (req, res) => {
+  // TODO
+
+  /**
+   *
+   * {
+   *      instructions: [
+   *          {
+   *              name: '...'
+   *          }
+   *      ]
+   * }
+   */
+  res.send('OK')
 })
 
 app.listen(port, () => {
