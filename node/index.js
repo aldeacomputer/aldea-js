159de9342190e5531ddfbcb301821d2f244fe060
Brenton Gunning <brentongunning@gmail.com>
2022-09-20 09:18:12 -0600

    Add express

--- node/index.js
+++ node/index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world')
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
