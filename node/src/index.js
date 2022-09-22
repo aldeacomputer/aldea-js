55615b7f21d83375c5f87430be397c0e57e31d7c
Brenton Gunning <brentongunning@gmail.com>
2022-09-21 23:50:32 -0600

    Add CORS

--- node/src/index.js
+++ node/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 import { VM } from '../../vm/vm/vm.js'
 import { Storage } from '../../vm/vm/storage.js'
@@ -11,6 +12,7 @@ const app = express()
 const port = 4000
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/status', (req, res) => {
   res.send('OK')
