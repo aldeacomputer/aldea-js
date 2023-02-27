4b42723560afc5e64071c464ce30aafc0cfff5b5
Migue <miguelfeliped@gmail.com>
2023-02-27 12:06:48 -0300

    fixed build server

--- node/src/index.js
+++ node/src/index.js
@@ -1,8 +1,9 @@
 import { buildApp } from './server.js'
+import {MomentClock} from "@aldea/vm";
 
 const port = process.env.PORT || 4000
 
-const { app } = buildApp()
+const { app } = buildApp(new MomentClock())
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
