a03647488baf9ce13abdeba3ec29993df084151e
migue <miguelfeliped@gmail.com>
2023-04-05 11:55:07 -0300

    exporting new code.

--- packages/sdk-js/src/index.ts
+++ packages/sdk-js/src/index.ts
@@ -29,3 +29,6 @@ export { base16, base64, bech32m } from './support/base.js'
 export * as blake3 from './support/blake3.js'
 export * as ed25519 from './support/ed25519.js'
 export * as util from './support/util.js'
+
+// SingleKeyWallet
+export * from './single-key-wallet.js'
