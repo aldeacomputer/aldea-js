aa92e77bab36ca9d0df9ec71503cdfaf01a45cfd
libitx <libitx@protonmail.com>
2023-04-17 14:25:23 +0100

    remove legacy signature class

--- packages/sdk-js/src/index.ts
+++ packages/sdk-js/src/index.ts
@@ -24,11 +24,8 @@ export {
 // Instructions exports
 export * as instructions from './instructions/index.js'
 
-// Exports support
+// Support exports
 export { base16, base64, bech32m } from './support/base.js'
 export * as blake3 from './support/blake3.js'
 export * as ed25519 from './support/ed25519.js'
 export * as util from './support/util.js'
-
-// Legacy exports - these will probably be removed
-export { Signature } from './signature.js'
