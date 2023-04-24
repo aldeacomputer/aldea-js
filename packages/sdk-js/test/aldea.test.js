a40dac53dea4892c9276296132c9347fe1064712
libitx <libitx@protonmail.com>
2023-04-24 19:03:26 +0100

    remove debug lin

--- packages/sdk-js/test/aldea.test.js
+++ packages/sdk-js/test/aldea.test.js
@@ -53,8 +53,6 @@ test('Builds a tx with every opcode and encodes/decodes consistently', async t =
     tx.signTo(keys.privKey)
   })
 
-  console.log(tx1.toHex())
-
   const tx2 = Tx.fromHex(tx1.toHex())
   t.true(tx1 instanceof Tx)
   t.true(tx2 instanceof Tx)
