1002823df62bf6bfe14fbb32394d8250a45ae629
libitx <libitx@protonmail.com>
2023-04-28 11:24:45 +0100

    starter kit bump

--- packages/create-aldea/templates/default/aldea/item.ts
+++ packages/create-aldea/templates/default/aldea/item.ts
@@ -1,17 +1,17 @@
 export class Item extends Jig {
   name: string;
-  prestige: u8;
+  power: u8;
 
   constructor(name: string) {
     super()
     this.name = name
-    this.prestige = 1
+    this.power = 1
   }
 
-  incPrestige(val: u8): void {
+  addPower(val: u8): void {
     if (val < 1 || val > 10) {
-      throw new Error('prestige can only be increased by 1-10')
+      throw new Error('power can only be increased by 1-10')
     }
-    this.prestige = min(this.prestige + val, 100)
+    this.power = min(this.power + val, 100)
   }
 }
