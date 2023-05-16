d668e27c9cd01698a7f9aca01a7e09570880de85
libitx <libitx@protonmail.com>
2023-05-16 13:54:09 +0100

    update vm deps and release version

--- packages/vm/cmd/compile-all.js
+++ packages/vm/cmd/compile-all.js
@@ -1,17 +1,17 @@
-import glob from 'glob';
+import { glob } from 'glob';
 import { fileURLToPath } from "url"
 import { compileFile } from "./compile-file.js"
 import { compileFileToTs } from './compile-file-to-ts.js'
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 
-glob(`${__dirname}../assembly/aldea/**/*.ts`, {}, async (err, fileList) => {
+await glob(`${__dirname}../assembly/aldea/**/*.ts`).then(async (fileList) => {
   for (const file of fileList) {
     await compileFile(file, `${__dirname}/../build/aldea`).catch(console.error)
   }
 })
 
-glob(`${__dirname}../assembly/builtins/*.ts`, {}, async (err, fileList) => {
+await glob(`${__dirname}../assembly/builtins/*.ts`).then(async (fileList) => {
   for (const file of fileList) {
     await compileFileToTs(file, `${__dirname}/../src/builtins`).catch(console.error)
   }
