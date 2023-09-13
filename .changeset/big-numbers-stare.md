4c66791d3c27811cc71e96657090e9ad621276c2
migue <miguelfeliped@gmail.com>
2023-09-13 11:01:56 -0300

    sending parameters as little endian

---
+++ .changeset/big-numbers-stare.md
@@ -0,0 +1,6 @@
+---
+"@aldea/compiler": minor
+"@aldea/vm": minor
+---
+
+Sending parameters as little endian instead of big endian.
--- packages/compiler/lib/aldea/arg-writer.ts
+++ packages/compiler/lib/aldea/arg-writer.ts
@@ -19,13 +19,13 @@ export class ArgWriter {
   }
 
   writeF32(val: f32): ArgWriter {
-    this.view.setFloat32(this.cursor, val)
+    this.view.setFloat32(this.cursor, val, true)
     this.cursor += 4
     return this
   }
 
   writeF64(val: f64): ArgWriter {
-    this.view.setFloat64(this.cursor, val)
+    this.view.setFloat64(this.cursor, val, true)
     this.cursor += 8
     return this
   }
@@ -37,19 +37,19 @@ export class ArgWriter {
   }
 
   writeI16(val: i16): ArgWriter {
-    this.view.setInt16(this.cursor, val)
+    this.view.setInt16(this.cursor, val, true)
     this.cursor += 2
     return this
   }
 
   writeI32(val: i32): ArgWriter {
-    this.view.setInt32(this.cursor, val)
+    this.view.setInt32(this.cursor, val, true)
     this.cursor += 4
     return this
   }
 
   writeI64(val: i64): ArgWriter {
-    this.view.setInt64(this.cursor, val)
+    this.view.setInt64(this.cursor, val, true)
     this.cursor += 8
     return this
   }
@@ -61,19 +61,19 @@ export class ArgWriter {
   }
 
   writeU16(val: u16): ArgWriter {
-    this.view.setUint16(this.cursor, val)
+    this.view.setUint16(this.cursor, val, true)
     this.cursor += 2
     return this
   }
 
   writeU32(val: u32): ArgWriter {
-    this.view.setUint32(this.cursor, val)
+    this.view.setUint32(this.cursor, val, true)
     this.cursor += 4
     return this
   }
 
   writeU64(val: u64): ArgWriter {
-    this.view.setUint64(this.cursor, val)
+    this.view.setUint64(this.cursor, val, true)
     this.cursor += 8
     return this
   }
