318c0394ad0c1c31e291f5396116f97881c99774
lebrunel <124721263+lebrunel@users.noreply.github.com>
2023-10-31 12:29:18 +0000

    remove bytes code from std lib

--- packages/compiler/lib/__tests__/aldea/arg-writer.spec.ts
+++ packages/compiler/lib/__tests__/aldea/arg-writer.spec.ts
@@ -1,64 +1,69 @@
-import { toHex } from '../../aldea/bytes'
 import { ArgWriter } from '../../aldea/arg-writer'
+
+function toHex(bytes: Uint8Array): string {
+  return bytes.reduce((hex, byte) => {
+    return hex += ('0' + byte.toString(16)).slice(-2)
+  }, '')
+}
 
 describe("ArgWriter", () => {
   test("encodes an i8", () => {
     const args = new ArgWriter(1)
     args.writeI8(127)
-    expect(toHex(args.buffer)).toBe('7f')
+    expect(toHex(Uint8Array.wrap(args.buffer))).toBe('7f')
   })
 
   test("encodes an i16", () => {
     const args = new ArgWriter(2)
     args.writeI16(32767)
-    expect(toHex(args.buffer)).toBe('ff7f')
+    expect(toHex(Uint8Array.wrap(args.buffer))).toBe('ff7f')
   })
 
   test("encodes an i32", () => {
     const args = new ArgWriter(4)
     args.writeI32(2147483647)
-    expect(toHex(args.buffer)).toBe('ffffff7f')
+    expect(toHex(Uint8Array.wrap(args.buffer))).toBe('ffffff7f')
   })
 
   test("encodes an i64", () => {
     const args = new ArgWriter(8)
     args.writeI64(9223372036854775807)
-    expect(toHex(args.buffer)).toBe('ffffffffffffff7f')
+    expect(toHex(Uint8Array.wrap(args.buffer))).toBe('ffffffffffffff7f')
   })
 
   test("encodes an u8", () => {
     const args = new ArgWriter(1)
     args.writeU8(255)
-    expect(toHex(args.buffer)).toBe('ff')
+    expect(toHex(Uint8Array.wrap(args.buffer))).toBe('ff')
   })
 
   test("encodes an u16", () => {
     const args = new ArgWriter(2)
     args.writeU16(65535)
-    expect(toHex(args.buffer)).toBe('ffff')
+    expect(toHex(Uint8Array.wrap(args.buffer))).toBe('ffff')
   })
 
   test("encodes an u32", () => {
     const args = new ArgWriter(4)
     args.writeU32(4294967295)
-    expect(toHex(args.buffer)).toBe('ffffffff')
+    expect(toHex(Uint8Array.wrap(args.buffer))).toBe('ffffffff')
   })
 
   test("encodes an u64", () => {
     const args = new ArgWriter(8)
     args.writeU64(18446744073709551615)
-    expect(toHex(args.buffer)).toBe('ffffffffffffffff')
+    expect(toHex(Uint8Array.wrap(args.buffer))).toBe('ffffffffffffffff')
   })
 
   test("encodes an f32", () => {
     const args = new ArgWriter(4)
     args.writeF32(123.123)
-    expect(toHex(args.buffer)).toBe('fa3ef642')
+    expect(toHex(Uint8Array.wrap(args.buffer))).toBe('fa3ef642')
   })
 
   test("encodes an f64", () => {
     const args = new ArgWriter(8)
     args.writeF64(12345.12345)
-    expect(toHex(args.buffer)).toBe('58a835cd8f1cc840')
+    expect(toHex(Uint8Array.wrap(args.buffer))).toBe('58a835cd8f1cc840')
   })
 })
