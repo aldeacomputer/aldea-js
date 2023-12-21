e128d48ab204f2131ac2bf2fb412792d73cac47a
lebrunel <124721263+lebrunel@users.noreply.github.com>
2023-12-21 15:07:52 +0000

    docs can use devnet 🎉

--- docs/.vitepress/store/files.ts
+++ docs/.vitepress/store/files.ts
@@ -14,7 +14,7 @@ const aldeaConfigStr = `
 /** @type {import('@aldea/cli').Config} */
 module.exports = {
   codeDir: 'aldea',
-  nodeUrl: 'https://node.aldea.computer'
+  nodeUrl: 'https://devnet.aldea.computer'
 }
 `.trim()
 
